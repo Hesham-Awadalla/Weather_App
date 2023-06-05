cd1387b64d09d09d0e0d71b4a849d3ae69a9ec9a
Hesham Awadalla <92855596+Hesham-Awadalla@users.noreply.github.com>
2023-06-05 23:53:41 +0200

    V1.1

---
+++ Weather App/Weather App/DBAccess.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using System.Text;
+
+namespace Weather_App
+{
+    class DBAccess
+    {
+
+        //Datenbank-Verbindungs-Objekt definiert, da es mehrfach innerhalb dieser Klasse benötigt wird.
+        SQLiteConnection sqlite_conn;
+
+        public DBAccess()
+        {
+            //--------------------------------------------------
+            //      Hier die Verbindung zur DB herstellen.
+            //--------------------------------------------------
+
+            string connectionString = "Data Source=" + Directory.GetCurrentDirectory() + @"\DB\cities;"; 
+
+            sqlite_conn = new SQLiteConnection(connectionString);
+        }
+
+        public string[] Check_City(string cityName, string countryName)
+        {
+            string city = null;
+            string latitude = null;
+            string longitude = null;
+            string country = null;
+
+            sqlite_conn.Open();
+
+            SQLiteDataReader sqlite_DataReader;
+            SQLiteCommand sqlite_Command;
+
+            sqlite_Command = sqlite_conn.CreateCommand();
+
+            string statement = $"SELECT lat, lng FROM cities WHERE city = '{cityName}' AND country = '{countryName}';";
+
+            sqlite_Command.CommandText = statement;
+
+            sqlite_DataReader = sqlite_Command.ExecuteReader();
+
+            while (sqlite_DataReader.Read())
+            {
+
+                latitude = sqlite_DataReader.GetString(0);
+                longitude = sqlite_DataReader.GetString(1);
+            }
+
+            sqlite_DataReader.Close();
+            sqlite_conn.Close();
+
+            string[] coordinates = {latitude, longitude};
+
+            return coordinates;
+        }
+    }
+}
--- Weather App/Weather App/MainWindow.xaml.cs
+++ Weather App/Weather App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,10 @@ namespace Weather_App
         public MainWindow()
         {
             InitializeComponent();
+
+            APICall.LocateMe();
+
+            //APICall.SearchMyCity("Cairo", "Egypt");
         }
     }
 }
